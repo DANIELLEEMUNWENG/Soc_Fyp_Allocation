eab9c12953ccbf6f1ffd9dcf808770bdefa19980
DANIELLEEMUNWENG <116176167+DANIELLEEMUNWENG@users.noreply.github.com>
2023-11-10 12:23:27 +0800

    able to assign topic to student

--- corAssign.php
+++ corAssign.php
@@ -1,5 +1,45 @@
 <?php
+// Include your database configuration file
+@include 'config.php';
 
+// Enable error reporting
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+// Check the database connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Start the session
+session_start();
+echo "it stops here (before session check)";
+
+// Check if the form is submitted
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    echo "Form submitted!";
+    $student_id = $_POST['student_id']; // Get the selected student's ID from the form
+    $selected_topic = $_POST['selected_topic']; // Get the selected topic from the form
+
+    if (!empty($student_id) && !empty($selected_topic)) {
+        // Insert the assignment into the tbl_topic_selection table
+        $insert_query = "INSERT INTO tbl_topic_selection (student_id, listTopics, assigned) VALUES ('$student_id', '$selected_topic', 1)";
+        
+        // Execute the query
+        if ($conn->query($insert_query) === TRUE) {
+            echo "Topic assigned successfully.";
+            // You can also update the availability of the topic in the tbl_topic table here if needed
+        } else {
+            echo "Error: " . $conn->error;
+        }
+    } else {
+        echo "Please select a student and a topic.";
+    }
+} else {
+    echo "Form not submitted.";
+}
+
+echo "it stops here (after form check)";
 ?>
 
 <!DOCTYPE html>
@@ -22,15 +62,47 @@
 <div class="container">
    <link href="style/taskbar.css" rel="stylesheet" type="text/css">
 
-<div class="topnav">
-  <a class="active" href="coordinator.php">Home</a>
-  <a href="corSelect.php">Select</a>
-  <a href="corView.php">View</a>
-  <a href="corAssign.php">Assign</a>
-  <a href="corDelete.php">Delete</a>
-</div>
+   <div class="topnav">
+      <a class="active" href="coordinator.php">Home</a>
+      <a href="corSelect.php">Select</a>
+      <a href="corView.php">View</a>
+      <a href="corAssign.php">Assign</a>
+      <a href="corDelete.php">Delete</a>
+   </div>
 
-</div>
+   <form method="post" action="">
+       <label for="student_id">Select Student:</label>
+       <select name="student_id">
+       <?php
+               // Retrieve a list of students from your database
+               $student_query = "SELECT id FROM tbl_student"; // Adjust this query
+               $student_result = $conn->query($student_query);
 
+               if ($student_result->num_rows > 0) {
+                   while ($row = $student_result->fetch_assoc()) {
+                       echo "<option value='" . $row['id'] . "'>" . $row['id'] . "</option>";
+                   }
+               }
+               ?>
+       </select>
+
+       <label for="selected_topic">Select Topic:</label>
+       <select name="selected_topic">
+       <?php
+               // Retrieve a list of available topics from your database
+               $topic_query = "SELECT listTopics FROM tbl_topic "; // Adjust this query
+               $topic_result = $conn->query($topic_query);
+
+               if ($topic_result->num_rows > 0) {
+                   while ($row = $topic_result->fetch_assoc()) {
+                       echo "<option value='" . $row['listTopics'] . "'>" . $row['listTopics'] . "</option>";
+                   }
+               }
+               ?>
+       </select>
+
+       <input type="submit" value="Assign Topic">
+   </form>
+</div>
 </body>
 </html>
